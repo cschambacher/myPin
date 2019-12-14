a9ff5a4cb2f7d1e14cb677dd137acacb21fa503b
corina <corina.schambacher@gmail.com>
2019-12-14 12:02:50 -0800

    BE and FE auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :api do
+    get 'users/index'
+    get 'users/show'
+    get 'users/create'
+    get 'users/destroy'
+    get 'users/update'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
