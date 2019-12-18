1653071fb633e2de8c7c645c0d8ebcd9a72a853d
corina <corina.schambacher@gmail.com>
2019-12-18 08:38:13 -0800

    fix heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducer/root_reducer';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== "production") {
     // must use 'require' (import only allowed at top of file)
-    const { logger } = require("redux-logger");
+    // const logger = require("redux-logger");
     middlewares.push(logger);
 }
 
@@ -15,7 +15,7 @@ const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer, 
         preloadedState, 
-        applyMiddleware(thunk, logger )
+        applyMiddleware(...middlewares )
         )
     );
 
